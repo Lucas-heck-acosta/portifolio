930d0de50d9038b2984ace392ea1c6ee21151e50
Lucas Acosta <lucas.heck@live.com>
2023-11-06 23:30:59 -0300

    add project card basic shape

---
+++ src/components/Project/index.tsx
@@ -0,0 +1,12 @@
+import Paragraph from '../Paragraph'
+import Title from '../Title'
+import { Card } from './styles'
+
+const Project = () => (
+  <Card>
+    <Title>Projeto lista de tarefas</Title>
+    <Paragraph type="secondary">Lista de tarefas feitas com vueJS</Paragraph>
+  </Card>
+)
+
+export default Project
